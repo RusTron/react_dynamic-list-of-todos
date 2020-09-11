d77d65d6545fb6cba3e63558ed2075813956ea1e
RusTron <rusan.tron@gmail.com>
2020-09-11 13:50:50 +0300

    Updates

---
+++ precache-manifest.e9f38451ca21c20c92a29a7c856ed237.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "598bf8032a8e179f9bd76825d13a223e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "382e2fdd0d2f705eac0d",
+    "url": "./static/css/main.341d21dd.chunk.css"
+  },
+  {
+    "revision": "901168486f84fbed8ebb",
+    "url": "./static/js/2.a2b3e2a6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.a2b3e2a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382e2fdd0d2f705eac0d",
+    "url": "./static/js/main.295cf60e.chunk.js"
+  },
+  {
+    "revision": "0c165204287428b65967",
+    "url": "./static/js/runtime-main.a37292e9.js"
+  }
+]);
